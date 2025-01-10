dcddee81ab8f71ff30f95c04ec3035cd6f9f3ed3
Eduardo Marques <edu.temarques@gmail.com>
2025-01-10 15:41:01 -0300

    feat: impl. user list feature

--- src/users/users-controller.ts
+++ src/users/users-controller.ts
@@ -54,5 +54,11 @@ export const userController = async (app: FastifyInstance) => {
     })
 
     // TODO: Implement the route to get all user registered
-    app.get('/', async (request, response) => { })
+    app.get('/', async (request, response) => {
+        // TODO: Load all users from the database
+        const users = await knex('users').select();
+
+        // TODO: Return success response with all users information
+        return response.status(200).send({ users });
+    })
 }
